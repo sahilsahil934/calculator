14c3c5ae7a602f328585c7e3e03e891ce450a890
iamsahil1910 <iamsahil1910@gmail.com>
2021-01-18 21:28:35 +0530

    css fixed

--- src/components/structure.js
+++ src/components/structure.js
@@ -178,34 +178,34 @@ class Structure extends React.Component {
                 <table className="table">
                         <tbody>
                             <tr>
-                            <td style={{backgroundColor: "lightgrey", color: "#45b3e0"}} onClick={() => this.clickedButton('C')}>C</td>
-                            <td style={{backgroundColor: "lightgrey", color: "#45b3e0"}} onClick={() => this.clickedButton('m')}>+/-</td>
-                            <td style={{backgroundColor: "lightgrey", color: "#45b3e0"}} onClick={() => this.clickedButton('%')}>%</td>
-                            <td style={{backgroundColor: "rgb(91, 98, 106)", color: "white"}} onClick={() => this.clickedButton('/')}>÷</td>
+                            <td style={{backgroundColor: "#f5f5f5", color: "darkgray"}} onClick={() => this.clickedButton('C')}>C</td>
+                            <td style={{backgroundColor: "#f5f5f5", color: "darkgray"}} onClick={() => this.clickedButton('m')}>+/-</td>
+                            <td style={{backgroundColor: "#f5f5f5", color: "darkgray"}} onClick={() => this.clickedButton('%')}>%</td>
+                            <td style={{backgroundColor: "#3c4b5e", color: "white"}} onClick={() => this.clickedButton('/')}>÷</td>
                             </tr>
                             <tr>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(7)}>7</td>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(8)}>8</td>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(9)}>9</td>
-                            <td style={{backgroundColor: "rgb(91, 98, 106)", color: "white"}} onClick={() => this.clickedButton('*')}>X</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(7)}>7</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(8)}>8</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(9)}>9</td>
+                            <td style={{backgroundColor: "#3c4b5e", color: "white"}} onClick={() => this.clickedButton('*')}>X</td>
                             </tr>
                             <tr>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(4)}>4</td>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(5)}>5</td>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(6)}>6</td>
-                            <td style={{backgroundColor: "rgb(91, 98, 106)", color: "white"}} onClick={() => this.clickedButton('-')}>-</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(4)}>4</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(5)}>5</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(6)}>6</td>
+                            <td style={{backgroundColor: "#3c4b5e", color: "white"}} onClick={() => this.clickedButton('-')}>-</td>
                             </tr>
                             <tr>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(1)}>1</td>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(2)}>2</td>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(3)}>3</td>
-                            <td style={{backgroundColor: "rgb(91, 98, 106)", color: "white"}} onClick={() => this.clickedButton('+')}>+</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(1)}>1</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(2)}>2</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(3)}>3</td>
+                            <td style={{backgroundColor: "#3c4b5e", color: "white"}} onClick={() => this.clickedButton('+')}>+</td>
                             </tr>
                             <tr>
-                            <td style={{color: "#45b3e0"}} onClick={() => this.clickedButton(0)}>0</td>
+                            <td style={{color: "darkgray"}} onClick={() => this.clickedButton(0)}>0</td>
                             <td></td>
-                            <td style={{color: "#45b3e0"}}  onClick={() => this.clickedButton('.')}>.</td>
-                            <td style={{backgroundColor: "#e6e600", color: "white"}}onClick={() => this.clickedButton('=')}>=</td>
+                            <td style={{color: "darkgray"}}  onClick={() => this.clickedButton('.')}>.</td>
+                            <td style={{backgroundColor: "#e7c313", color: "white"}}onClick={() => this.clickedButton('=')}>=</td>
                             </tr>
                         </tbody>
                         </table>
